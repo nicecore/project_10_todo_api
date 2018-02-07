829e1563aa9b20794c8ba583bbd3a65805910d8c
Adam Cameron <adamdcameron@gmail.com>
2018-02-07 11:58:29 -0600

    final commit

--- test.py
+++ test.py
@@ -21,29 +21,29 @@ class TodoTests(unittest.TestCase):
 
 
     def test_item(self):
-        """Post, get, update & delete an item"""
+        """Tests for GET, POST, PUT, and DELETE methods"""
         resp = self.app.post('/api/v1/todos', data={'name': 'Mow the yard'})
         self.assertEqual(resp.status_code, 201)
 
-        # Item in main list
+        # Test GET request on all Todos
         resp = self.app.get('/api/v1/todos')
         self.assertEqual(resp.status_code, 200)
         self.assertIn('Mow the yard', str(resp.data))
         todo = Todo.get(name='Mow the yard')
 
-        # Get item
+        # Test GET method on one Todo
         resp = self.app.get('/api/v1/todos/{}'.format(todo.id))
         self.assertEqual(resp.status_code, 200)
         self.assertIn('Mow the yard', str(resp.data))
 
-        # Edit Item
+        # Test PUT method on one Todo
         resp = self.app.put('/api/v1/todos/{}'.format(todo.id),
                               data={'name': 'Mow the lawn'})
         self.assertIn('/api/v1/todos/{}'.format(todo.id),
                       resp.headers['Location'])
         query = Todo.get(name='Mow the lawn')
 
-        # Delete Item
+        # Test DELETE method on one Todo
         resp = self.app.delete('/api/v1/todos/{}'.format(todo.id))
         self.assertEqual(resp.status_code, 204)
 
